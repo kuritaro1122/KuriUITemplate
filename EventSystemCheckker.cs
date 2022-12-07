7f191ed042a44e21a57cdc08af3bd3669c6f8ff9
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2022-12-08 01:15:08 +0900

    update

--- EventSystemCheckker.cs
+++ EventSystemCheckker.cs
@@ -9,8 +9,12 @@ namespace KuriTaro.EventSystemExpansion {
     public class EventSystemCheckker : MonoBehaviour {
         [SerializeField] EventSystem eventSystem;
         [SerializeField] bool ForceSelect = true;
-        [SerializeField] GameObject selectCurrent = null;
+        private GameObject selectCurrent = null;
         private GameObject preSelectGameObject = null;
+        private static bool buttonDown = false;
+        void Start() {
+            if (this.eventSystem == null) this.eventSystem = EventSystem.current;
+        }
         void Update() {
             if (this.eventSystem != null) this.eventSystem = EventSystem.current;
             GameObject current = this.eventSystem.currentSelectedGameObject;
@@ -23,16 +27,17 @@ namespace KuriTaro.EventSystemExpansion {
             }
             if (current != null) this.preSelectGameObject = current;
 
-            if (!DefaultAcceptButton()) {
-                if (selectCurrent != null) {
+            if (DefaultAcceptButton()) {
+                if (selectCurrent != null && !buttonDown) {
+                    buttonDown = true;
                     if (this.selectCurrent.GetComponent<IPointerClickHandler>() == null) return;
                     var eventData = new PointerEventData(eventSystem);
                     eventData.position = this.selectCurrent.transform.position;
                     eventData.pressPosition = this.selectCurrent.transform.position;
                     eventData.button = PointerEventData.InputButton.Left;
                     ExecuteEvents.Execute<IPointerClickHandler>(this.selectCurrent, eventData, (handler, ev) => handler.OnPointerClick((PointerEventData)ev));
-                }
-            }
+                } 
+            } else buttonDown = false;
         }
         protected virtual bool DefaultAcceptButton() {
             return false;
